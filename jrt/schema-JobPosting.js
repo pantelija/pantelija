fd01501304aa79002492c924ac1c63829be2a5ba
pantelija <pantelicnd@gmail.com>
2020-01-28 02:44:46 +0100

    jrt

---
+++ jrt/schema-JobPosting.js
@@ -0,0 +1,44 @@
+/* https://www.joberty.rs/posao/senior-salesforce-developer/namics/150 */
+
+<script type="application/ld+json">
+{
+"@context":"http://schema.org",
+	"@type":"JobPosting",
+	"datePosted":"2020-01-10T08:58:50-08:00",
+	"description":"As the leading full-service digital agency in the German-speaking region, we offer our customers from all industries the full range of IT and Web Services: E-Commerce, Mobile Business Apps, Digital Communications, Websites and Customer Relationshi...",
+	"baseSalary":
+		{ 
+			"@type": "MonetaryAmount",
+			"currency": "EUR",
+			"value": "1000",
+			"unitCode": "Month" 
+		},
+	"employmentType":"FULL_TIME",
+	"industry":"Software Outsourcing",
+	"jobLocationType":"TELECOMMUTE",
+	"jobLocation":
+		{	
+			"address":
+			{
+				"@type":"PostalAddress",
+				"addressCountry":"Serbia",
+				"addressRegion":"Beograd",
+				"streetAddress":"",
+				"postalCode":"11000",
+				"addressLocality":"Beograd"
+			}
+		},
+	"title":"Senior Salesforce Developer",
+	"image":"",
+	"occupationalCategory":"15-1132.00 Software Developers, Applications",
+	"workHours":"40 hours per week",
+	"validThrough":"2020-01-31T09:58:50-07:00",
+	"hiringOrganization":
+		{
+			"@type":"Organization",
+			"name":"Namics",
+			"sameAs":"https://www.linkedin.com/company/namics-ag/",
+			"logo":"https://www.joberty.rs/images/logo-kompanije.png"
+		}
+} 
+</script>
