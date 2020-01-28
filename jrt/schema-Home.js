58a23fc181968f9ede52b2e10fb3fdfe7109abc0
pantelija <pantelicnd@gmail.com>
2020-01-28 03:06:29 +0100

    scheme

---
+++ jrt/schema-Home.js
@@ -0,0 +1,31 @@
+<script type="application/ld+json">
+            {
+                "@context": "http://schema.org",
+                "@type": "Organization",
+                "name": "Joberty",
+                "legalName" : "Joberty technologies doo",
+                "alternateName": "Joberty Srbija",
+                "url": "https://www.joberty.rs",
+                "logo": "https://www.joberty.rs/images/joberty_logo_coral.png",
+                "foundingDate": "2019",
+                "address": {
+                    "@type": "PostalAddress",
+                    "streetAddress": "Veljka Dugosevica 54",
+                    "addressLocality": "Beograd",
+                    "addressRegion": "Srbija",
+                    "postalCode": "11000",
+                    "addressCountry": "RS"
+                },
+                "contactPoint": {
+                    "@type": "ContactPoint",
+                    "contactType": "customer support",
+                    "telephone": "+381 69 12345 33",
+                    "email": "hello@joberty.rs"
+                },
+                "sameAs": [
+                    "https://www.linkedin.com/company/joberty/",
+                    "https://www.facebook.com/joberty.rs/",
+                    "https://www.instagram.com/joberty.rs/"
+                ]
+            }
+        </script>
---
+++ jrt/schema-Kompanija-regular.js
@@ -0,0 +1,21 @@
+/* https://www.joberty.rs/it-kompanija/139/two-desperados/o-kompaniji */
+
+<script type="application/ld+json">
+            {
+                "@context": "http://schema.org",
+                "@type": "Organization",
+                "name": "Two Desperados",
+                "legalName" : "Two Desperados ",
+                "alternateName": "Two Desperados",
+                "url": "https://www.twodesperados.com/",
+                "logo": "https://www.joberty.rs/images/logo-kompanije.png",
+                "foundingDate": "2011",
+                "address": {
+                    "@type": "PostalAddress",
+                    "addressLocality": "Beograd",
+                    "addressRegion": "Srbija",
+                    "postalCode": "11000",
+                    "addressCountry": "RS"
+                }
+            }
+</script>
